fc8ebf8273c320774c500a5d71cdc42323dd9a42
heqian li <heqian@ix-trusty.cs.uoregon.edu>
2015-12-09 15:05:18 -0800

    update

--- findairline.php
+++ findairline.php
@@ -1,13 +1,9 @@
 <?php
 
 include('connectionData.txt');
-try
-{
-$dbh = new PDO('mysql:host='.$server.';port='.$port.';dbname='.$dbname, $user, $pass);
-} catch (PDOException $e) {
-print $e->getMessage();
-exit;
-}
+
+$conn = mysqli_connect($server, $user, $pass, $dbname, $port)
+or die('Error connecting to MySQL server.');
 
 ?>
 <html>
@@ -15,21 +11,20 @@ exit;
 <title>Another Simple PHP-MySQL Program</title>
 </head>
 
-<body bgcolor="grey">
+<body bgcolor="lightgrey">
 
 <hr>
 
 <?php
 
-$arrive = $_POST['arrive'];
-$departure = $_POST['departure'];
+$a = $_POST['arrive'];
+$d = $_POST['departure'];
 
 $query = "select airline_id, model, seats, corp_code
 from airline_info a join plane_info p on a.airline_id = p.airline_info_airline_id join corporation c on p.plane_id = c.plane_info_plane_id
 where arrive = ";
-$query = $query."'".$arrive."' ";
-$query = $query." and departure = ";
-$query = $query."'".$departure."' ";
+$query = $query."'".$a."' and departure = ";
+$query = $query."'".$d."' ";
 
 ?>
 
@@ -37,3 +32,40 @@ $query = $query."'".$departure."' ";
 The query:
 <p>
 <?php
+print $query;
+?>
+
+<hr>
+<p>
+Result of query:
+<p>
+
+<?php
+$result = mysqli_query($conn, $query)
+or die(mysqli_error($conn));
+
+print "<pre>";
+while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
+  {
+    print "\n";
+    print "<font size = '6' color='green'>airline number: $row[airline_id] Plane model: $row[model] Seats: $row[seats]</font> ";
+    print "\n";
+    print "<font size ='6' color='yellow'>airline: $row[corp_code]</font>";
+  }
+print "</pre>";
+
+mysqli_free_result($result);
+
+mysqli_close($conn);
+
+?>
+
+<p>
+<hr>
+
+<p>
+<a href="findCustState.txt" >Contents</a>
+of the PHP program that created this page. 	 
+ 
+</body>
+</html>
